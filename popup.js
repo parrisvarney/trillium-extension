abb87dbad78df9cc22dbcee60b2efcc14c6a80c8
Parris Varney <pvarney@copatient.com>
2015-11-10 10:00:03 -0500

    Updated a bare bones version for new website

--- popup.js
+++ popup.js
@@ -1,59 +1,29 @@
+$.get('http://www.trilliumbrewing.com/?no-cache=' + new Date().getTime(), function(html) {
+    var fortPointBeers = [];
+    var fortPointText = '';
 
-if (localStorage['showUpcoming']*1) {
-    document.getElementById("up-next").style.display = "block";
-    document.getElementById("up-next-label").style.display = "block";
-}
+    $(html).find('h3').each(function() {
+        if ($(this).text().match(/AVAILABLE AT FORT POINT/i)) {
+            $(this).closest('.sqs-block').next().find('img').each(function() {
+                var beerName = $(this).attr('alt');
+                var beerFormat = [];
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://www.trilliumbrewing.com/wordpress_blog/beers/find-trillium-2/?no-cache=' + new Date().getTime(), true);
-xhr.onload = function() {
-    var hoursDiv         = document.getElementById('hours'),
-        growlersDiv      = document.getElementById('growlers'),
-        bottlesDiv       = document.getElementById('bottles'),
-        upNextDiv        = document.getElementById('up-next'),
-        lastUpdatedDiv   = document.getElementById('last-updated'),
-        retailHoursStart = this.responseText.indexOf('Retail Hours'),
-        growlersStart    = this.responseText.indexOf('Growler Fills'),
-        bottlesStart     = this.responseText.indexOf('Bottles'),
-        upNextStart      = this.responseText.indexOf('Up Next'),
-        bottleShopsStart = this.responseText.indexOf('BOTTLE SHOPS'),
-        lastUpdatedStart = this.responseText.indexOf('UPDATED'),
-        retailHours,
-        growlers,
-        bottles,
-        upNext,
-        lastUpdated;
-
-    retailHours = this.responseText.substring(retailHoursStart, growlersStart).match(/<h3>(.*?)<\/h3>/ig);
-    growlers    = this.responseText.substring(growlersStart, bottlesStart).match(/<h3>(.*?)<\/h3>/ig);
-    bottles     = this.responseText.substring(bottlesStart, upNextStart).match(/<h3>(.*?)<\/h3>/ig);
-    upNext      = this.responseText.substring(upNextStart, bottleShopsStart).match(/<h3>(.*?)<\/h3>/ig);
-    lastUpdated = this.responseText.substring(lastUpdatedStart + 8, lastUpdatedStart + 40).split('</span>')[0];
-
-    hoursDiv.innerHTML = '';
-    retailHours.forEach(function(r) {
-        hoursDiv.innerHTML = hoursDiv.innerHTML + r;
+                $(this).closest('a').next().find('.summary-metadata-container--below-content .summary-metadata--primary .summary-metadata-item--tags a').each(function() {
+                    beerFormat.push($(this).text());
+                });
+                fortPointBeers.push({name: beerName, format: beerFormat});
+            });
+        }
     });
 
-    growlersDiv.innerHTML = '';
-    growlers.forEach(function(r) {
-        growlersDiv.innerHTML = growlersDiv.innerHTML + r;
-    });
-
-    bottlesDiv.innerHTML = '';
-    bottles.forEach(function(r) {
-        bottlesDiv.innerHTML = bottlesDiv.innerHTML + r;
-    });
-
-    lastUpdatedDiv.innerHTML = 'Updated: ' + lastUpdated;
-
-
-    if (localStorage['showUpcoming'] *1) {
-        upNextDiv.innerHTML = '';
-        upNext.forEach(function (r) {
-            upNextDiv.innerHTML = upNextDiv.innerHTML + r;
-        });
+    for (var i=0; i<fortPointBeers.length; i++) {
+        fortPointText +=
+            "<div class='available-beer-div'>"+
+            fortPointBeers[i].name+
+            "<span class='format-span'>"+
+            fortPointBeers[i].format.join(' ')+
+            "</span></div>";
     }
-};
 
-xhr.send();
+    $('#fort-point').html(fortPointText);
+});
