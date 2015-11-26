cbb705083c3b7e5172184c17a082feb6f7e74dfa
Parris Varney <pvarney@copatient.com>
2015-11-26 11:20:09 -0500

    Adding beer icons, and updated date.  Made background image fixed, renamed package.zip to Archive.zip.  Bump manifest version.

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
 $.get('http://www.trilliumbrewing.com/?no-cache=' + new Date().getTime(), function(html) {
     var fortPointBeers = [];
     var fortPointText = '';
+    var fortPointUpdated = new Date(0);
 
     $(html).find('h3').each(function() {
         if ($(this).text().match(/AVAILABLE AT FORT POINT/i)) {
+            fortPointUpdated = new Date($(this).closest('[data-type=page]').data('updated-on'));
+
             $(this).closest('.sqs-block').next().find('img').each(function() {
                 var beerName = $(this).attr('alt');
+                var beerImg  = $(this).data('src');
                 var beerFormat = [];
 
                 $(this).closest('a').next().find('.summary-metadata-container--below-content .summary-metadata--primary .summary-metadata-item--tags a').each(function() {
                     beerFormat.push($(this).text());
                 });
-                fortPointBeers.push({name: beerName, format: beerFormat});
+                fortPointBeers.push({name: beerName, imgSrc: beerImg, format: beerFormat});
             });
         }
     });
@@ -19,6 +23,7 @@ $.get('http://www.trilliumbrewing.com/?no-cache=' + new Date().getTime(), functi
     for (var i=0; i<fortPointBeers.length; i++) {
         fortPointText +=
             "<div class='available-beer-div'>"+
+            "<img src='"+fortPointBeers[i].imgSrc+"'/>"+
             fortPointBeers[i].name+
             "<span class='format-span'>"+
             fortPointBeers[i].format.join(' ')+
@@ -26,4 +31,5 @@ $.get('http://www.trilliumbrewing.com/?no-cache=' + new Date().getTime(), functi
     }
 
     $('#fort-point').html(fortPointText);
+    $('#last-updated').html(fortPointUpdated.toLocaleString());
 });
